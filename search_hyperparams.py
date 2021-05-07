1781fa91d312dff0227c1913975c253ac37fdc76
Iain Rolland <imr27@cam.ac.uk>
2021-05-07 10:51:41 +0100

    Wrote experiment to investigate OOD detection performance across tests using 2 randomly sampled classes to be OOD

--- search_hyperparams.py
+++ search_hyperparams.py
@@ -36,7 +36,7 @@ def launch_training_job(parent_dir, job_name, params):
 if __name__ == "__main__":
     # Load the "reference" parameters from .json file
     args = parser.parse_args()
-    json_path = "experiments/OOD_Detection_seed_1/params.json"
+    json_path = "experiments/Sampled_OOD_classes/params.json"
     assert os.path.isfile(json_path), "No json configuration file found at {}".format(json_path)
     params = Params(json_path)
 
@@ -45,12 +45,15 @@ if __name__ == "__main__":
     # channels = [8, 16, 32, 64]
     # l2_loss_coefficients = [1e-4, 1e-3, 1e-2]
     # seeds = [0, 1, 2, 3]
+    ood_classes_list = [[7, 12], [1, 10], [13, 1], [5, 4], [9, 13], [8, 12], [4, 2], [2, 13], [16, 13], [16, 11]]
     model_names = ["GCN", "Drop-GCN", "S-GCN", "S-BGCN", "S-BGCN-T", "S-BGCN-T-K"]
 
-    for model_name in model_names:
-        # Modify the relevant parameter in params
-        params.model = model_name
+    for ood_classes in ood_classes_list:
+        for model_name in model_names:
+            # Modify the relevant parameter in params
+            params.ood_classes = ood_classes
+            params.model = model_name
 
-        # Launch job (name has to be unique)
-        job_name = "model_{}".format(model_name)
-        launch_training_job(args.parent_dir, job_name, params)
+            # Launch job (name has to be unique)
+            job_name = "ood_classes_{}{}_model_{}".format(*ood_classes, model_name)
+            launch_training_job(args.parent_dir, job_name, params)
