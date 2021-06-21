608f3ffc9eaa0c592aec2607e61ee426df5c186c
Iain Rolland <imr27@cam.ac.uk>
2021-06-21 09:51:13 +0100

    Wrote models classes which define methods to allow train.py to switch in different types seamlessly

--- models.py
+++ models.py
@@ -1,20 +1,95 @@
+from abc import ABC
 import tensorflow as tf
 import numpy as np
 from spektral.transforms import LayerPreprocess, AdjToSpTensor
 from spektral.layers import GCNConv
+from spektral.models import GCN
+from spektral.data import SingleLoader
+import os
 
 from losses import SquareErrorDirichlet, T_SquareErrorDirichlet, K_SquareErrorDirichlet, T_K_SquareErrorDirichlet
+import utils
 
 
-class GCN:
+class Model(ABC):
+    network = None
+
+    @staticmethod
+    def output_activation(x):
+        raise NotImplementedError
+
+    def get_network(self, params, n_inputs, n_outputs):
+        raise NotImplementedError
+
+    def compile_network(self, params):
+        raise NotImplementedError
+
+
+class GraphModel(Model, ABC):
     transforms = [LayerPreprocess(GCNConv), AdjToSpTensor()]
-    loss = tf.losses.CategoricalCrossentropy()
-    output_activation = "softmax"
+
+    def get_network(self, params, n_inputs, n_outputs):
+        return GCN(n_labels=n_outputs, channels=params.channels, n_input_channels=n_inputs,
+                   output_activation=self.output_activation, l2_reg=params.l2_loss_coefficient)
+
+    def fit_network(self, params, dataset):
+        # weights_va, weights_te = (
+        #     utils.mask_to_weights(mask).astype(np.float32)
+        #     for mask in (dataset.mask_va, dataset.mask_te)
+        # )
+        weights_tr, weights_va = [utils.weight_by_class(dataset[0].y, mask) for mask in
+                                  [dataset.mask_tr, dataset.mask_va]]
+
+        loader_tr = SingleLoader(dataset, sample_weights=weights_tr)
+        loader_va = SingleLoader(dataset, sample_weights=weights_va)
+        history = self.network.fit(
+            loader_tr.load(),
+            steps_per_epoch=loader_tr.steps_per_epoch,
+            validation_data=loader_va.load(),
+            validation_steps=loader_va.steps_per_epoch,
+            epochs=params.epochs,
+            callbacks=[tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=params.patience,
+                                                        restore_best_weights=True),
+                       tf.keras.callbacks.ModelCheckpoint(os.path.join(params.directory, self.__name__ + ".h5"),
+                                                          monitor="val_loss", save_best_only=True,
+                                                          save_weights_only=True)]
+        )
+        return history
 
 
-class S_BGCN:
-    transforms = [LayerPreprocess(GCNConv), AdjToSpTensor()]
-    loss = SquareErrorDirichlet()
+class S_BMLP(Model, ABC):
+    def __init__(self):
+        self.transforms = None
+        self.__name__ = S_BMLP.__name__
+
+    def get_network(self, params, n_inputs, n_outputs):
+        self.network = MLP(params.hidden_units_1, params.hidden_units_2, params.l2_loss_coefficient,
+                           n_outputs, self.output_activation)
+        return self.network
+
+    def fit_network(self, params, dataset):
+        x, y = dataset[0].x, dataset[0].y
+        x_tr, x_va = x[dataset.mask_tr], x[dataset.mask_va]
+        y_tr, y_va = y[dataset.mask_tr], y[dataset.mask_va]
+        history = self.network.fit(
+            x=x_tr, y=y_tr, sample_weight=utils.weight_by_class(y_tr, np.ones(len(y_tr))),
+            batch_size=256,
+            validation_data=(x_va, y_va, utils.weight_by_class(y_va, np.ones(len(y_va)))),
+            epochs=params.epochs,
+            callbacks=[tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=params.patience,
+                                                        restore_best_weights=True),
+                       tf.keras.callbacks.ModelCheckpoint(os.path.join(params.directory, self.__name__ + ".h5"),
+                                                          monitor="val_loss", save_best_only=True,
+                                                          save_weights_only=True)]
+        )
+        return history
+
+    def compile_network(self, params):
+        self.network.compile(
+            optimizer=tf.keras.optimizers.Adam(params.learning_rate),
+            loss=SquareErrorDirichlet(),
+            weighted_metrics=["acc"]
+        )
 
     @staticmethod
     def output_activation(x):
@@ -23,14 +98,66 @@ class S_BGCN:
         return tf.exp(x) + 1
 
 
-class S_BGCN_T:
-    transforms = [LayerPreprocess(GCNConv), AdjToSpTensor()]
-    loss = None
+class VanillaGCN(GraphModel):
+    def __init__(self):
+        self.__name__ = VanillaGCN.__name__
+
+    def get_network(self, params, n_inputs, n_outputs):
+        self.network = super().get_network(params, n_inputs, n_outputs)
+        return self.network
+
+    def compile_network(self, params):
+        self.network.compile(
+            optimizer=tf.keras.optimizers.Adam(params.learning_rate),
+            loss=tf.losses.CategoricalCrossentropy(),
+            weighted_metrics=["acc", get_metric(self.network, params.l2_loss_coefficient)]
+        )
+
+    @staticmethod
+    def output_activation(x):
+        return tf.keras.activations.softmax(x)
+
+
+class S_BGCN(GraphModel):
+    def __init__(self):
+        self.__name__ = S_BGCN.__name__
+
+    def get_network(self, params, n_inputs, n_outputs):
+        self.network = super().get_network(params, n_inputs, n_outputs)
+        return self.network
+
+    def compile_network(self, params):
+        self.network.compile(
+            optimizer=tf.keras.optimizers.Adam(params.learning_rate),
+            loss=SquareErrorDirichlet(),
+            weighted_metrics=["acc", get_metric(self.network, params.l2_loss_coefficient)]
+        )
+
+    @staticmethod
+    def output_activation(x):
+        """ makes sure model output >=1 since a) in the absence of evidence the subjective logic framework dictates
+        alpha is 1 and b) in the presence of evidence alpha is greater than 1"""
+        return tf.exp(x) + 1
+
+
+class S_BGCN_T(GraphModel):
 
     def __init__(self, gcn_prob_path, teacher_coefficient):
-        self.loss = T_SquareErrorDirichlet(np.load(gcn_prob_path).astype(np.float32), teacher_coefficient)
+        self.gcn_prob = np.load(gcn_prob_path).astype(np.float32)
+        self.teacher_coefficient = teacher_coefficient
         self.__name__ = S_BGCN_T.__name__
 
+    def get_network(self, params, n_inputs, n_outputs):
+        self.network = super().get_network(params, n_inputs, n_outputs)
+        return self.network
+
+    def compile_network(self, params):
+        self.network.compile(
+            optimizer=tf.keras.optimizers.Adam(params.learning_rate),
+            loss=T_SquareErrorDirichlet(self.gcn_prob, self.teacher_coefficient),
+            weighted_metrics=["acc", get_metric(self.network, params.l2_loss_coefficient)]
+        )
+
     @staticmethod
     def output_activation(x):
         """ makes sure model output >=1 since a) in the absence of evidence the subjective logic framework dictates
@@ -38,17 +165,29 @@ class S_BGCN_T:
         return tf.exp(x) + 1
 
 
-class S_BGCN_T_K:
-    transforms = [LayerPreprocess(GCNConv), AdjToSpTensor()]
-    loss = None
+class S_BGCN_T_K(GraphModel):
 
     def __init__(self, gcn_prob_path, alpha_prior_path, teacher_coefficient, alpha_prior_coefficient):
-        self.loss = T_K_SquareErrorDirichlet(np.load(gcn_prob_path).astype(np.float32),
-                                             np.load(alpha_prior_path).astype(np.float32),
-                                             teacher_coefficient,
-                                             alpha_prior_coefficient)
+        self.gcn_prob = np.load(gcn_prob_path).astype(np.float32)
+        self.alpha_prior = np.load(alpha_prior_path).astype(np.float32)
+        self.teacher_coefficient = teacher_coefficient
+        self.alpha_prior_coefficient = alpha_prior_coefficient
         self.__name__ = S_BGCN_T_K.__name__
 
+    def get_network(self, params, n_inputs, n_outputs):
+        self.network = super().get_network(params, n_inputs, n_outputs)
+        return self.network
+
+    def compile_network(self, params):
+        self.network.compile(
+            optimizer=tf.keras.optimizers.Adam(params.learning_rate),
+            loss=T_K_SquareErrorDirichlet(self.gcn_prob,
+                                          self.alpha_prior,
+                                          self.teacher_coefficient,
+                                          self.alpha_prior_coefficient),
+            weighted_metrics=["acc", get_metric(self.network, params.l2_loss_coefficient)]
+        )
+
     @staticmethod
     def output_activation(x):
         """ makes sure model output >=1 since a) in the absence of evidence the subjective logic framework dictates
@@ -56,28 +195,57 @@ class S_BGCN_T_K:
         return tf.exp(x) + 1
 
 
-class S_BGCN_K:
-    transforms = [LayerPreprocess(GCNConv), AdjToSpTensor()]
-    loss = None
+class S_BGCN_K(GraphModel):
 
     def __init__(self, alpha_prior_path, alpha_prior_coefficient):
-        self.loss = K_SquareErrorDirichlet(np.load(alpha_prior_path).astype(np.float32),
-                                           alpha_prior_coefficient)
+        self.alpha_prior = np.load(alpha_prior_path).astype(np.float32)
+        self.alpha_prior_coefficient = alpha_prior_coefficient
         self.__name__ = S_BGCN_K.__name__
+
+    def get_network(self, params, n_inputs, n_outputs):
+        self.network = super().get_network(params, n_inputs, n_outputs)
+        return self.network
+
+    def compile_network(self, params):
+        self.network.compile(
+            optimizer=tf.keras.optimizers.Adam(params.learning_rate),
+            loss=K_SquareErrorDirichlet(self.alpha_prior,
+                                        self.alpha_prior_coefficient),
+            weighted_metrics=["acc", get_metric(self.network, params.l2_loss_coefficient)]
+        )
 
     @staticmethod
     def output_activation(x):
         """ makes sure model output >=1 since a) in the absence of evidence the subjective logic framework dictates
         alpha is 1 and b) in the presence of evidence alpha is greater than 1"""
         return tf.exp(x) + 1
+
+
+class MLP(tf.keras.Model):
+
+    def get_config(self):
+        raise NotImplementedError
+
+    def __init__(self, hidden_units_1, hidden_units_2, l2_loss_coefficient, n_outputs, output_activation):
+        super(MLP, self).__init__()
+        self.hidden_1 = tf.keras.layers.Dense(hidden_units_1, activation="relu",
+                                              kernel_regularizer=tf.keras.regularizers.l2(l2_loss_coefficient))
+        self.hidden_2 = tf.keras.layers.Dense(hidden_units_2, activation="relu",
+                                              kernel_regularizer=tf.keras.regularizers.l2(l2_loss_coefficient))
+        self.output_layer = tf.keras.layers.Dense(n_outputs, activation=output_activation)
+
+    def call(self, inputs, **kwargs):
+        x = self.hidden_1(inputs)
+        x = self.hidden_2(x)
+        return self.output_layer(x)
 
 
 def get_model(params):
-    supported_models = dict(zip(["GCN", "Drop-GCN", "S-GCN", "S-BGCN", "S-BGCN-T", "S-BGCN-K", "S-BGCN-T-K"],
-                                [GCN, GCN, S_BGCN, S_BGCN, S_BGCN_T, S_BGCN_K, S_BGCN_T_K]))
+    supported_models = dict(zip(["GCN", "Drop-GCN", "S-GCN", "S-BGCN", "S-BGCN-T", "S-BGCN-K", "S-BGCN-T-K", "S-BMLP"],
+                                [VanillaGCN, VanillaGCN, S_BGCN, S_BGCN, S_BGCN_T, S_BGCN_K, S_BGCN_T_K, S_BMLP]))
     try:
-        if params.model not in ["S-BGCN-T", "S-BGCN-K", "S-BGCN-T-K"]:
-            return supported_models[params.model]
+        if params.model in ["GCN", "Drop-GCN", "S-GCN", "S-BGCN", "S-BMLP"]:
+            return supported_models[params.model]()  # for models which don't take parameters in their __init__
         elif params.model == "S-BGCN-T":
             try:
                 return supported_models[params.model](params.teacher_file_path, params.teacher_coefficient)
@@ -94,7 +262,7 @@ def get_model(params):
                 message += "-alpha_prior_path (a file path pointing to saved alpha prior array)\n"
                 message += "-alpha_prior_coefficient (float coeff. for KLD(output, alpha prior) loss [default: 0.001])"
                 raise AttributeError(message)
-        else:
+        else:  # i.e. params.model == "S-BGCN-T-K":
             try:
                 return supported_models[params.model](params.teacher_file_path, params.alpha_prior_path,
                                                       params.teacher_coefficient, params.alpha_prior_coefficient)
@@ -108,3 +276,10 @@ def get_model(params):
     except KeyError:
         raise ValueError(
             "{} was not a recognised dataset. Must be one of {}.".format(params.model, "/".join(supported_models)))
+
+
+def get_metric(model, weight):
+    def gcn_conv_0_l2_reg_loss(y_true, y_pred):
+        return tf.nn.l2_loss(model.layers[1].kernel) * weight
+
+    return gcn_conv_0_l2_reg_loss
